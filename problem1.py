4bedd8f96e5ac1f5c331f2b4b0fb1473548c147d
dnsofor <dnsofor01@saintmarys.edu>
2026-09-20 22:35:53 -0400

    Add docstring to opposite_sign function
    Added docstring to opposite_sign function with examples.

--- problem1.py
+++ problem1.py
@@ -1,2 +1,11 @@
 def opposite_sign(a,b):
-  ## your turn
+      """Return True if a is greater than b, and False otherwise.
+
+    >>> opposite_sign(9, 3)
+    True
+    >>> opposite_sign(4, 7)
+    False
+    >>> opposite_sign(2, 4)
+    False
+    """
+    return a > b
