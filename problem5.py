221d90d43fa488a905297d432729b1323d63fe21
dnsofor <dnsofor01@saintmarys.edu>
2026-09-21 00:09:08 -0400

    Add jacket-wearing logic to wears_jacket function
    Implement logic to determine if Belle wears a jacket based on temperature and rain.

--- problem5.py
+++ problem5.py
@@ -1,2 +1,10 @@
 def wears_jacket(temp, raining):
-  ### your solution here
+    """Return True if Belle wears a jacket.
+    >>> wears_jacket(50, False)
+    True
+    >>> wears_jacket(70, True)
+    True
+    >>> wears_jacket(70, False)
+    False
+    """
+    return temp < 60 or raining
