10df2a04557e51efe9e3f5fdc25a42c047c7c4bf
dnsofor <dnsofor01@saintmarys.edu>
2026-09-21 00:20:53 -0400

    Clean up docstring in divisible_by_m function
    Removed example test cases from the docstring.

--- problem2.py
+++ problem2.py
@@ -1,18 +1,11 @@
 def divisible_by_m(n,m):
   """
   Return True if n is divisible by m, and False otherwise.
-
   >>> divisible_by_m(3,2)
   False
   >>> divisible_by_m(0,4)
   True
   >>> divisible_by_m(-6, 2)
   True
-  >>> divisible_by_m(10, 5)
-  True
-  >>> divisible_by_m(7, 3)
-  False
-  >>> divisible_by_m(5, 0)
-  False
   """
   return n % m == 0
