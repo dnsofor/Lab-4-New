e4847a59c025058fadc079fcb915482b4308ff4b
dnsofor <dnsofor01@saintmarys.edu>
2026-09-20 23:56:07 -0400

    Add pythagorean_triples function implementation
    Implement function to check Pythagorean triples.

--- problem3.py
+++ problem3.py
@@ -1,2 +1,10 @@
 def pythagorean_triples(a,b,c):
-  ## your code here
+    """Return True if a^2 + b^2 equals c^2, and False otherwise.
+    >>> pythagorean_triples(3, 4, 5)
+    True
+    >>> pythagorean_triples(5, 12, 13)
+    True
+    >>> pythagorean_triples(2, 3, 4)
+    False
+    """
+    return a ** 2 + b ** 2 == c ** 2
