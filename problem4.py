716171896a7be2b51477271d175a89cf2f0a678b
dnsofor <dnsofor01@saintmarys.edu>
2026-09-21 00:07:30 -0400

    Add logic to check if three sides form a triangle
    Implement triangle validation logic in is_triangle function.

--- problem4.py
+++ problem4.py
@@ -1,2 +1,8 @@
 def is_triangle(a,b,c):
-  ## your turn
+   """Return True if a, b, and c can form a triangle.
+   >>> is_triangle(3, 4, 5)
+   True
+   >>> is_triangle(2, 3, 10)
+   False
+   """
+   return a + b > c and a + c > b and b + c > a
